f9eb83c3236848abf70fe5b3f5d68f468cd85fdd
lucastheldl <lucastheldl@gmail.com>
2023-07-29 13:06:36 -0300

    feat: create Transactions table and styles

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHightlight,
+  TransactionContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div className="">
       <Header />
       <Summary />
+
+      <TransactionContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightlight variant="income">R$ 12.200,00</PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>22/03/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Pizza</td>
+              <td>
+                <PriceHightlight variant="outcome">- R$ 45,00</PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>22/03/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionContainer>
     </div>
   );
 }
